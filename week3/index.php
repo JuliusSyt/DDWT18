ddbb9d67e08ed926c932c70594c18ff5d55832bd
JuliusSyt <julius.s92@gmail.com>
2018-12-05 16:09:53 +0100

    site funcionality with postman fixing

--- week3/index.php
+++ week3/index.php
@@ -25,40 +25,20 @@ $router->mount('/api', function() use($router, $db){
     http_content_type('application/json');
 
     $router->get('/series', function() use ($db) {
-        echo json_encode(get_series($db));
+        echo (get_series($db));
     });
 
     $router->get('/series', function() use ($db) {
-        echo json_encode(count_series($db));
+        echo (count_series($db));
     });
 
     /* GET for reading individual series */
     $router->get('/series/(\d+)', function($id) use($db) {
         // Retrieve and output information
         $serie_info = get_serieinfo($db, $id);
-        return json_encode($serie_info);
-
+        return ($serie_info);
     });
 
-    /* DELETE for individual serie */
-    $router->delete('/series/(\d+)', function($id) use($db) {
-        // Retrieve and output information
-        remove_serie($db, $id);
-    });
-
-    /* Post for individual serie */
-    $router->post('/series', function($_POST) use($db) {
-        // Retrieve and output information
-        add_serie($db, $_POST);
-    });
-
-    /* Update for inidividual serie */
-    $router->put('/series/(\d+)', function($id) use($db) {
-        // Fake $_PUT
-        $_PUT = array();
-        parse_str(file_get_contents('php://input'), $_PUT);
-
-    });
 });
 
 $router->set404(function() {
@@ -66,7 +46,6 @@ $router->set404(function() {
          echo'Your requested page was not found';
     }
 });
-
 
 /* Run the router */
 $router->run();
